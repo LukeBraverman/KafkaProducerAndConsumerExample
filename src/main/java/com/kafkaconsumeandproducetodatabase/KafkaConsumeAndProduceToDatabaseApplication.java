a755194c764fcc66f60bbcd8b7512eb043da94f9
lbrav <l.braverman.l@live.co.uk>
2021-09-02 09:47:09 +0100

    Innitial commit

---
+++ src/main/java/com/kafkaconsumeandproducetodatabase/KafkaConsumeAndProduceToDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.kafkaconsumeandproducetodatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaConsumeAndProduceToDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaConsumeAndProduceToDatabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kafkaconsumeandproducetodatabase/KafkaConsumeAndProduceToDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kafkaconsumeandproducetodatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumeAndProduceToDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
